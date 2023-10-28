8fbd185a0c064f3c0f9cef3bb607cf4dc8942bbe
uday 1221 <saiuday901@gmail.com>
2023-10-28 09:52:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const facts = [
+    "Honey never spoils.",
+    "A group of flamingos is called a 'flamboyance'.",
+    "The Eiffel Tower can be 15 cm taller during the summer due to thermal expansion.",
+    "Octopuses have three hearts.",
+    "The longest time between two twins being born is 87 days."
+];
+
+function generateRandomFact() {
+    const factIndex = Math.floor(Math.random() * facts.length);
+    const randomFact = facts[factIndex];
+    document.getElementById("fact-display").textContent = randomFact;
+    alert(randomFact);
+}
+
+document.getElementById("generate-button").addEventListener("click", generateRandomFact);
